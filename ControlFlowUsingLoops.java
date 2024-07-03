42c4e8bfb9f5b33a870c27d2478d3dc4234755c7
Rai <151567119+rnrhltn@users.noreply.github.com>
2024-07-04 01:34:54 +0800

    Create ControlFlowUsingLoops.java

---
+++ ControlFlowUsingLoops.java
@@ -0,0 +1,12 @@
+public class ControlFlowUsingLoops{
+  public static void main(String[] args){
+    int check_number = 10;
+    String message;
+      for(int i = 0; i <= check_number; i++){
+          message = (i%2==0) ? i+ "is even number"  : i+ "is odd number" ;
+          message = (i%2==0) ? "even" : "odd";
+          System.out.println(message);
+      }
+  }
+}
+  
